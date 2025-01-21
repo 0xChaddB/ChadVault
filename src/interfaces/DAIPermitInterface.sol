d71b5135c15cffda34c48edcf5d9c1d5dc431d25
0xChaddB <0xChaddB@proton.me>
2025-01-22 00:15:27 +0100

    chore: changed interface name

--- src/interfaces/DAIPermitInterface.sol
+++ src/interfaces/DAIPermitInterface.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
-interface IDaiPermit {
+interface DAIPermitInterface {
     function permit(
         address holder,
         address spender,
