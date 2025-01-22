0014514ce740be5f37db73b9593c5c5d9db06104
0xChaddB <0xChaddB@proton.me>
2025-01-22 19:09:30 +0100

    feat: restarting the project

--- src/interfaces/IStrategy.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.28;
-
-interface IStrategy {
-    function deposit(uint256 amount) external;
-    function withdraw(uint256 amount) external;
-    function totalAssets() external view returns (uint256);
-}
