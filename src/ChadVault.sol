6cd1b317cf30cb4272031f4bde20d818b94ff039
0xChaddB <0xChaddB@proton.me>
2025-01-22 19:34:56 +0100

    feat: add vault constructor

--- src/ChadVault.sol
+++ src/ChadVault.sol
@@ -1,2 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
+
+import {ERC4626, IERC20, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+
+
+contract ChadVault is ERC4626 {
+
+    constructor(IERC20 _asset)   
+        ERC20("vChadDAI", "VCD") 
+        ERC4626(_asset) 
+    {
+        require(address(_asset) != address(0), "Invalid asset address");
+    }
+}
