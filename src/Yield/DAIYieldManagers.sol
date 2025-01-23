fce1018b3d87b8eae2a14cb9457930f7e7ac373c
0xChaddB <0xChaddB@proton.me>
2025-01-23 22:43:14 +0100

    feat: updated implementation

--- src/Yield/DAIYieldManagers.sol
+++ src/Yield/DAIYieldManagers.sol
@@ -5,7 +5,7 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {IPoolAddressesProvider} from "@aave/contracts/interfaces/IPoolAddressesProvider.sol";
 import {IPool} from "@aave/contracts/interfaces/IPool.sol";
 import {IYieldManager} from "../IYieldManager.sol";
-
+// Since YieldManager have no balance, how can ensure they transfer the tokens to the Vault, and how the vault react from tokens being send to him ?
 contract DAIYieldManager is IYieldManager {
     address public immutable vault;
     address public immutable dai;
@@ -27,21 +27,22 @@ contract DAIYieldManager is IYieldManager {
     }
 
     //Invest DAI into Aave
-    function invest(uint256 amount) external override onlyVault() {
-        IERC20(dai).safeTransferFrom(vault, address(this), amount); //Pull DAI from Vault
-        IERC20(dai).approve(address(aavePool), amount); //Aaprove Aave to spend DAI
-        aavePool.deposit(dai, amount, address(this), 0); // Deposit into Aave
+    function invest(uint256 amount) external onlyVault {
+        // Ensure the Vault has approved the tokens for this contract
+        asset.safeTransferFrom(vault, address(this), amount);
 
-        totalInvestedAssets += amount; //Update accounting
+        // Supply the tokens to Aave
+        pool.supply(address(asset), amount, address(this), 0);
 
         emit LogInvested(amount);
     }
 
-    function withdraw(uint256 amount) external override onlyVault() {
-        uint256 withdrawn = aavePool.withdraw(dai, amount, vault); //Withdraw from Aave to the vault
-        totalInvestedAssets -= withdrawn; //Update accounting
 
-        emit LogWithdrawn(withdrawn);
+    function withdraw(uint256 amount) external onlyVault {
+        // Withdraw the tokens from Aave
+        pool.withdraw(address(asset), amount, vault);
+
+        emit LogWithdrawn(amount);
     }
 
     // Returns the total DAI currently managed by the YieldManager
