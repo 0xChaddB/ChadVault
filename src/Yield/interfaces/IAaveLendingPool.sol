8aea9d3d836e30ee1309e211548de2a0bd682312
0xChaddB <0xChaddB@proton.me>
2025-01-23 22:31:46 +0100

    deleted folder

--- src/Yield/interfaces/IAaveLendingPool.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
-
-interface IAaveLendingPool {
-    function deposit(address asset, uint256 amount, address onBehalfOf, uint16 referralCode) external;
-    function withdraw(address asset, uint256 amount, address to) external returns (uint256);
-    function getReserveData(address asset) external view returns (uint256 liquidityRate);
-}
