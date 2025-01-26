8cf954a233f276538dce6f30ca04b1ddfe00b15d
0xChaddB <0xChaddB@proton.me>
2025-01-26 13:29:32 +0100

    feat: basic implementation

--- test/DAIYieldManager.t.sol
+++ test/DAIYieldManager.t.sol
@@ -2,12 +2,9 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {DAIYieldManager} from "../src/DAIYieldManager.sol"; 
+import {DAIYieldManager} from "../src/Yield/DAIYieldManager.sol"; 
 
 contract DAIYieldManagerTest is Test {
     DAIYieldManager public dAIYieldManager;
 
-    function setUp() public {
-        dAIYieldManager = new DAIYieldManager();
-    }
 }
