47c19f6b1df729dc5df0d8de33dca81f9bea32cf
0xChaddB <0xChaddB@proton.me>
2025-02-06 21:56:32 +0100

    feat: first aavestrategy commit

---
+++ src/strategies/AaveStrategy.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {IPool} from "@aave/contracts/interfaces/IPool.sol";
+import {IPoolAddressesProvider} from "@aave/contracts/interfaces/IPoolAddressesProvider.sol";
+import {IStrategy} from "./interfaces/IStrategy.sol";
+
+contract AaveStrategy is Ownable, ReentrancyGuard, IStrategy {
+
+
+
+}
